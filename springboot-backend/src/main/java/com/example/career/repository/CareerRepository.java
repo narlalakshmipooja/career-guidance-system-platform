cc85619cce84eaf836f3a08a4ae3366603e232de
Narla Lakshmi pooja <narlalakshmipooja05@gmail.com>
2026-04-28 09:24:37 +0530

    Remove backend directories - keep only frontend

--- springboot-backend/src/main/java/com/example/career/repository/CareerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.career.repository;
-
-import com.example.career.model.Career;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CareerRepository extends JpaRepository<Career, Long> {
-}
--- springboot-backend/src/main/java/com/example/careerguidance/repository/CareerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.careerguidance.repository;
-
-import com.example.careerguidance.model.Career;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CareerRepository extends JpaRepository<Career, Long> {
-}
